0702668ba0649a9be2fe9f9f351171f50d158c0e
William Kosasih <williamkosasih19@gmail.com>
2017-12-14 13:33:13 +0700

    millet

--- millet.mk
+++ millet.mk
@@ -16,9 +16,6 @@
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-# Get non-open-source specific aspects
-$(call inherit-product-if-exists, vendor/samsung/matisse-common/matisse-common-vendor.mk)
-
 ## We are a tablet, not a phone
 PRODUCT_CHARACTERISTICS := tablet
 
